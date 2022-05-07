8bc696902dd28d11b4d746209be8e97a549455b2
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2022-05-07 16:48:04 -0300

    Feat: methods PUT and DELETE

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -2,9 +2,16 @@ package com.dio.santander.bankline.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +39,25 @@ public class CorrentistaController {
 	@PostMapping
 	public void save (@RequestBody NovoCorrentista novoCorrentista) {
 		service.save(novoCorrentista);
+	}
+	
+	//Atualiza alguma postagem por meio do ID
+	@PutMapping
+	public ResponseEntity<Correntista> putPostagem (@Valid @RequestBody Correntista correntista){
+		return repository.findById(correntista.getId())
+				.map(resposta -> ResponseEntity.ok().body(repository.save(correntista)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	// Deleta uma postagem por meio do ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCorrentista(@PathVariable int id){
+		
+		return repository.findById(id).map(resposta -> {
+			repository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		})
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
